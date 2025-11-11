db61d88e622668e23146d11ecb175ab6ae725285
Kibiwott <bk.kibiwott@gmail.com>
2025-11-11 16:16:41 +0300

    Updated

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,3 +1,34 @@
 export const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <>
+      <section className="py-20 md:py-10  bg-white dark:bg-gray-900 flex flex-col md:flex-row items-center justify-between">
+        <div className="flex flex-col md:flex-row items-center  max-w-6xl mx-auto">
+          <div className="md:w-1/2 text-center md:text-left">
+          <h1 className="text-4xl font-bold text-gray-800 dark:text-white md:text-5xl">
+            Welcome to Sweet Delights
+          </h1>
+          <p className="mt-4 text-gray-600 dark:text-gray-300 md:text-lg">
+            Indulge in the finest selection of desserts crafted with love and
+            passion.
+          </p>
+          <div className="mt-6">
+            <a
+              href="#explore"
+              className="px-5 py-3 bg-pink-500 text-white rounded-lg hover:bg-pink-600"
+            >
+              Explore Our Menu
+            </a>
+          </div>          
+        </div>
+        <div>
+            <img
+              src="https://i.pinimg.com/736x/50/87/0b/50870b77b0ab6ed969a9a43b2d4ddfbc.jpg"
+              alt="Delicious Dessert"
+              className="w-full  max-w-lg h-auto mt-10 md:mt-0 md:ml-4 rounded-lg shadow-lg"
+            />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router";
 
 export const Navbar = () => {
   return (
-    <div className="navbar bg-- shadow-sm">
+    <div className="navbar sticky top-0 z-50 bg-white dark:bg-gray-400 shadow-md px-4 md:px-8">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -24,7 +24,7 @@ export const Navbar = () => {
           </div>
           <ul
             tabIndex={-1}
-            className="menu menu-sm dropdown-content text-2xl text-white bg-gray-700 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            className="menu menu-sm dropdown-content text-3xl text-black bg-red-200 rounded-box z-1 mt-3 w-52 p-2 shadow"
           >
             <li>
               <NavLink to="/">Home</NavLink>
@@ -49,10 +49,13 @@ export const Navbar = () => {
             </li>
           </ul>
         </div>
-        <a className="btn btn-ghost text-2xl font-bold"><span className="text-3xl text-purple-500">Sweet</span>Delights</a>
+        <a className="btn btn-ghost text-xl md:text-xl font-bold">
+          <span className="text-2xl md:text-3xl text-purple-500">Sweet</span>
+          Delights
+        </a>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal  text-2xl  px-1">
+        <ul className="menu menu-horizontal  text-xl font-bold px-2">
           <li>
             <NavLink to="/">Home</NavLink>
           </li>
@@ -76,9 +79,9 @@ export const Navbar = () => {
           </li>
         </ul>
       </div>
-      <div className="navbar-end">
+      {/* <div className="navbar-end">
         <a className="btn">Order now</a>
-      </div>
+      </div> */}
     </div>
   );
 };
