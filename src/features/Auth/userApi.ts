e80b718031a7d6bee35832573d7d093984033018
Kibiwott <bk.kibiwott@gmail.com>
2025-11-11 23:16:20 +0300

    Updatd data

---
+++ src/features/Auth/userApi.ts
@@ -0,0 +1,31 @@
+import { ApiUrl } from "../../utils/apiUrl";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export type Tuser = {
+  user_id: number;
+  name: string;
+  email: string;
+  password: string;
+  phone: string;
+  address: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export const userApi = createApi({
+  reducerPath: "userApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: ApiUrl,
+  }),
+  tagTypes: ["User"],
+  endpoints: (builder) => ({
+    createUser: builder.mutation<Tuser, Partial<Tuser>>({
+      query: (body) => ({
+        url: "/users/register",
+        method: "POST",
+        body,
+      }),
+      invalidatesTags: ["User"],
+    }),
+  }),
+});
