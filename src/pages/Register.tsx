db9e2067d0fcea5f70ec4d755e836ceed3e2abc6
Kibiwott <bk.kibiwott@gmail.com>
2025-11-11 22:05:28 +0300

    Updated

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,6 +1,8 @@
 import { Footer } from "../components/footer/Footer";
 import { Navbar } from "../components/navbar/Navbar";
 import { useForm, type SubmitHandler } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
 import * as yup from "yup";
 
 type inputData = {
@@ -26,7 +28,9 @@ export const Register = () => {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<inputData>();
+  } = useForm<inputData>({
+    resolver: yupResolver(schema),
+  });
   const onsubmit: SubmitHandler<inputData> = (data) => {
     console.log(data);
   };
@@ -34,30 +38,42 @@ export const Register = () => {
   return (
     <>
       <Navbar />
-      <div>
-        <h1>Register Page</h1>
-        <div>
-          <form onSubmit={handleSubmit(onsubmit)}>
-            <input type="text" placeholder="Firstname" {...register("name")} />
-            <input type="email" placeholder="Email" {...register("email")} />
+      <div className="mt-10 mb-10 w-full md:w-1/2 sm:mx-auto  py-6 rounded-b-lg shadow-lg p-6">
+        <h1 className="text-center text-2xl font-bold">Register Page</h1>
+        <div className="mt-6 px-6 bg-gray-100 rounded-md py-6">
+          <form onSubmit={handleSubmit(onsubmit)} className="flex flex-col gap-3 "> 
+            <input type="text" placeholder="Firstname" {...register("name")} className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+             />
+            <span className="text-red-500 text-xs" >{errors.name?.message}</span>
+            <input type="email" placeholder="Email" {...register("email")} className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+             />
+            <span>{errors.email?.message}</span>
             <input
               type="text"
               placeholder="Phone Number"
               {...register("phone")}
+              className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
             />
-            <input type="text" placeholder="Address" {...register("address")} />
+            <span className="text-red-500 text-xs" >{errors.phone?.message}</span>
+            <input type="text" placeholder="Address" {...register("address")} className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+             />
+            <span className="text-red-500 text-xs" >{errors.address?.message}</span>  
             <input
               type="password"
               placeholder="Password"
               {...register("password")}
+              className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
             />
+            <span className="text-red-500 text-xs" >{errors.password?.message}</span>
             <input
               type="password"
               placeholder="Confirm Password"
               {...register("confirmPassword")}
+              className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
             />
-
-            <button type="submit">Register</button>
+            <span className="text-red-500 text-xs" >{errors.confirmPassword?.message}</span>
+            <button type="submit" className="w-full rounded-md bg-indigo-500 px-3 py-1.5 text-base text-white hover:bg-indigo-600 focus:outline-2 focus:outline-indigo-500 sm:text-sm/6"
+>Register</button>
           </form>
         </div>
       </div>
