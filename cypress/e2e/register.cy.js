25e24873e50fcfe11649a1e5d3655e450fd2a14e
Kibiwott <bk.kibiwott@gmail.com>
2025-12-04 00:21:09 +0300

    Updated

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -53,4 +53,54 @@ describe("Register test", () => {
         cy.contains(/User registered successfully!/i).should("be.visible");
 
     });
+    it("Should fail for unmatching passwords", () => {
+        cy.contains(/Create Your Account/i).should("be.visible");    
+        cy.getDataTest("register-heading").should("be.visible");
+
+        cy.intercept('POST', '/users/register',{
+            statusCode: 201,
+            body: {message: "User created successfully. Verification code sent to bk.kibiwott@gmail.com."
+            }
+
+        }) .as('registerUser');
+
+        cy.getDataTest("register-name").as("name-input");
+        cy.get("@name-input")
+            .should("be.visible")
+            .should("have.attr", "type", "text")
+            .type("Test User");
+        
+        cy.getDataTest("register-email").as("email-input");
+        cy.get("@email-input")
+            .should("be.visible")
+            .should("have.attr", "type", "email")
+            .type("bk.kibiwott@gmail.com");
+        cy.getDataTest("register-password").as("password-input");
+        cy.get("@password-input")
+            .should("be.visible")
+            .should("have.attr", "type", "password")
+            .type("Test@1234");
+        cy.getDataTest("register-confirm-password").as("confirmPassword-input");
+        cy.get("@confirmPassword-input")
+            .should("be.visible")
+            .should("have.attr", "type", "password")
+            .type("Test@12345");
+        cy.getDataTest("register-address").as("address-input");
+        cy.get("@address-input")
+            .should("be.visible")
+            .should("have.attr", "type", "text")
+            .type("123 Test St, Test City");
+        cy.getDataTest("register-phone").as("phone-input");
+        cy.get("@phone-input")
+            .should("be.visible")
+            .should("have.attr", "type", "text")
+            .type("1234567890");
+        cy.getDataTest("register-submit").as("registerSubmit-button");
+        cy.get("@registerSubmit-button")
+            .should('contain', 'Create Account')
+            .should("not.be.disabled")        
+            .click();       
+        cy.contains(/Passwords must match/i).should("be.visible");
+
+    });
 })
