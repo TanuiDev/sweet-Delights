a9187b84c5e78c8e8be87b31811eed0c4cb79017
Kibiwott <bk.kibiwott@gmail.com>
2025-12-10 12:23:12 +0300

    Updated

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,17 +3,17 @@ export const Footer = () => {
     <footer className="footer sm:footer-horizontal bg-base-300 text-base-content p-10">
       <nav>
         <h6 className="footer-title">Services</h6>
-        <a className="link link-hover">Branding</a>
-        <a className="link link-hover">Design</a>
+        <a className="link link-hover">Baking</a>
+        <a className="link link-hover">Designing</a>
         <a className="link link-hover">Marketing</a>
-        <a className="link link-hover">Advertisement</a>
+        
       </nav>
       <nav>
         <h6 className="footer-title">Company</h6>
         <a className="link link-hover">About us</a>
         <a className="link link-hover">Contact</a>
-        <a className="link link-hover">Jobs</a>
-        <a className="link link-hover">Press kit</a>
+        <a className="link link-hover">Testimonials</a>
+      
       </nav>
       <nav>
         <h6 className="footer-title">Social</h6>
